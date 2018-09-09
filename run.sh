fd0354c09896737bbfc083b16d5b1c76ac1c6f29
oelabed <Oussama.El.Abed@fircosoft.com>
2018-09-09 03:00:04 +0200

    change logo

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-sudo docker build -t chez-lui-env .
+# sudo docker build -t chez-lui-env .
 sudo docker run --rm -v $(pwd):/ionicapp chez-lui-env ./scripts/build.sh
-# docker run --rm -d -v $(pwd):/ionicapp chez-lui-env tail -f /dev/null
-# docker exec -e COLUMNS="`tput cols`" -e LINES="`tput lines`" -ti 10c6493cd8b5 bash
+sudo rm -rf /vagrant/*.apk
+cp /home/vagrant/work/repos/pooc/chez-lui-package/chezlui-app/platforms/android/app/build/outputs/apk/armv7/release/*.apk /vagrant
+sudo rm -Rf node_modules
+npm i
